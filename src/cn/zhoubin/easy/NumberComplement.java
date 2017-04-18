21c9d0ca3a0e29c5f682ba70ab3399557f040b0c
xzbin <738150158@qq.com>
2017-04-18 20:33:57 +0800

    20170418pc

--- src/cn/zhoubin/easy/NumberComplement.java
+++ src/cn/zhoubin/easy/NumberComplement.java
@@ -1,8 +1,6 @@
 package cn.zhoubin.easy;
 
-/**
- * Created by Administrator on 2017/4/17.
- */
+
 public class NumberComplement {
     public int findComplement(int num) {
 
@@ -12,20 +10,10 @@ public class NumberComplement {
     public static void main(String[] args) {
         int a = 5;
         int b = ~a;
-        System.out.println( Integer.highestOneBit(a));
-        for (int i = 0; i < 32; i++) {
-            System.out.print(a & 1);
-            a = a >>> 1;
-        }
-
-//
+        System.out.println( a +"  "+b);
 //        for (int i = 0; i < 32; i++) {
-//            System.out.print(b & 1);
-//            b = b >>> 1;
+//            System.out.print(a & 1);
+//            a = a >>> 1;
 //        }
-
-
-
-//        System.out.println(new Integer(~a));
     }
 }
